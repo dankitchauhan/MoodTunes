901ecc66f53ccb9222525823058f966a13826660
Ankit Chauhan <ankitchauhan246@gmail.com>
2023-03-25 15:42:54 +0530

    emotion detector code added

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, Response
+from camera import *
+
+app = Flask(__name__)
+
+headings = ("Name","Album","Artist")
+featuredPlaylist = music_rec()
+featuredPlaylist = featuredPlaylist.head(15)
+@app.route('/')
+def index():
+    print(featuredPlaylist.to_json(orient='records'))
+    return render_template('index.html', headings=headings, data=featuredPlaylist)
+
+def gen(camera):
+    while True:
+        global featuredPlaylist
+        frame, featuredPlaylist = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/t')
+def gen_table():
+    return featuredPlaylist.to_json(orient='records')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
